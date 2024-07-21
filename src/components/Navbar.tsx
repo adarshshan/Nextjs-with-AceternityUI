5ad1d2d93c689d41dfbd1cea04d1dbb7125ed6e7
Adarsh <adarshshanu3@gmail.com>
2024-07-21 15:00:40 +0530

    changes

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+const Navbar = ({ className }: { className?: string }) => {
+    const [active, setActive] = useState<string | null>(null);
+    return (
+        <div
+            className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)} >
+            <Menu setActive={setActive}>
+                <Link href={'/'}>
+                    <MenuItem setActive={setActive} active={active} item="Home">
+                        <HoveredLink href="/courses"> Cources</HoveredLink>
+                    </MenuItem>
+                </Link>
+                <MenuItem setActive={setActive} active={active} item="Our Cources">
+                    <div className="flex flex-col space-y-8 text-sm">
+                        <HoveredLink href="/courses">All Cources</HoveredLink>
+                        <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+                        <HoveredLink href="/courses">Advanced Composition</HoveredLink>
+                        <HoveredLink href="/courses">Song Writing</HoveredLink>
+                        <HoveredLink href="/courses">Music Production</HoveredLink>
+                    </div>
+                </MenuItem>
+                <MenuItem setActive={setActive} active={active} item="Contact Us">
+                    <HoveredLink href="/courses"> Contact Us</HoveredLink>
+                </MenuItem>
+            </Menu>
+        </div>
+    )
+}
+
+export default Navbar
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 import type { Config } from "tailwindcss";
+
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
 
 const config: Config = {
   content: [
@@ -6,8 +25,24 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: '0',
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: '1',
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -15,6 +50,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
 export default config;
