77935362520ec7c1e5a8bc072988dc2bae0d33ba
Adarsh <adarshshanu3@gmail.com>
2024-07-22 11:16:08 +0530

    completed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,25 @@
+'use client'
 import React from 'react'
+import { Meteors } from "@/components/ui/meteors";
 
 const page = () => {
     return (
-        <div>
-            test 2
+        <div className='w-full pt-32 min-h-screen '>
+            <div className="flex justify-center text-center">
+                <div>
+                    <h1 className='text-4xl text-white font-bold'>Contact Us</h1>
+                    <p className='max-w-lg mt-4'>We're here to help with any questions about our courses, programs, or events. Feach out and let us know how we can assist you in your musical journey.</p>
+                   <div className="mt-4 mb-4">
+                   <input type="email" className=' mt-5 mb-5 w-1/2 py-4 px-10 rounded-full' placeholder='your email address' />
+                   </div>
+                   <div className="mt-4 mb-4">
+                   <input type="text" className='  mb-5 w-1/2 py-4 px-10 rounded-full' placeholder='Your message' />
+                   </div>
+                   <input type="submit" className='  mb-5 w-1/2 py-4 px-10 rounded-full bg-blue-500' placeholder='Your message' />
+                </div>
+            </div>
+
+            <Meteors number={20} />
         </div>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,11 +52,20 @@ const config: Config = {
   theme: {
     extend: {
       animation: {
+        "meteor-effect": "meteor 5s linear infinite",
         scroll:
           "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
         spotlight: "spotlight 2s ease .75s 1 forwards",
       },
       keyframes: {
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
+          },
+        },
         scroll: {
           to: {
             transform: "translate(calc(-50% - 0.5rem))",
